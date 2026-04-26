2f2e26e57062e9f0c4efaf120a3722e30bcf9c9b
Greedrache <greedrache@gmail.com>
2026-04-26 10:35:13 +0200

    Wird nicht mehr gebraucht

--- Javascript/headerlinks.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const headerLinks = document.getElementById('headerlinks');
-
-menuToggle.addEventListener('click', () => {
-    headerLinks.classList.toggle('active');
-});
